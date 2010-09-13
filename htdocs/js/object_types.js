57638ede3ce6d4d044689ab08bb2511572a892f1
richtaur <richtaur@gmail.com>
2010-09-12 22:45:02 -0700

    just playin' around, and added some tiny icons for an idea. when you pick up an item it could generate a little sprinkle of tiny icons that float upward and fade out. thinking just some visuals to make items look more like they are gathered

--- htdocs/js/object_types.js
+++ htdocs/js/object_types.js
@@ -549,7 +549,7 @@ o.cube = {
 	moveChangeDelay: 1000,
 
 	damage: 25,
-	hitPoints: 1000,
+	hitPoints: 500,
 	speed: 15,
 	worth: 10000,
 	
@@ -640,7 +640,7 @@ o.gel = {
 	moveChangeElapsed: 0,
 	moveChangeDelay: 1000,
 
-	damage: 25,
+	damage: 5,
 	hitPoints: 10,
 	speed: 200,
 	worth: 10,
@@ -683,7 +683,7 @@ o.superclops = {
 	moveChangeDelay: 1000,
 
 	damage: 35,
-	hitPoints: 200,
+	hitPoints: 600,
 	speed: 25,
 	worth: 1000,
 
