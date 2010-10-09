83cc8ba1ffec7655d748a7941b7d121df8b2341d
Matt Hackett <richtaur@gmail.com>
2010-10-08 19:56:37 -0700

    oops forgot to take out test gel

--- htdocs/js/engine.js
+++ htdocs/js/engine.js
@@ -487,7 +487,6 @@ proto.initWaves = function horde_Engine_proto_initWaves () {
 	w.addSpawnPoint(0, 1000);
 	w.addSpawnPoint(1, 1000);
 	w.addSpawnPoint(2, 1000);
-	w.addObjects(0, "gel", 1);
 	w.addObjects(0, "bat", 1);
 	w.addObjects(1, "bat", 1);
 	w.addObjects(2, "bat", 1);
