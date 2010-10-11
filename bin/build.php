686322d1d52dd670e1520322c9c967d005bb9bd4
geoffb <gmblair@gmail.com>
2010-10-10 17:22:43 -0700

    New build which includes the font folder

--- bin/build.php
+++ bin/build.php
@@ -50,6 +50,7 @@ function buildWeb ($version) {
 	exec("cp -r {$htdocs}/img {$web_root}");
 	exec("cp -r {$htdocs}/css {$web_root}");
 	exec("cp -r {$htdocs}/lib {$web_root}");
+	exec("cp -r {$htdocs}/font {$web_root}");
 	exec("cp -r {$htdocs}/sound {$web_root}");
 	exec("cp -r {$htdocs}/favicon.ico {$web_root}");
 	exec("cp -r {$htdocs}/robots.txt {$web_root}");	
