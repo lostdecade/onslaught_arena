67a9b35e0e7a2ad0d7a9e6799101a9b4de169093
geoffb <gmblair@gmail.com>
2010-11-08 01:28:22 -0800

    sigh, removing wave testing hack

--- htdocs/js/engine.js
+++ htdocs/js/engine.js
@@ -630,11 +630,11 @@ proto.initWaves = function horde_Engine_proto_initWaves () {
 	};
 	
 	// Wave testing code...
-	
+	/*
 	var testWave = 40;
 	this.waveHack = true;
 	this.currentWaveId = (testWave - 2);
-	
+	*/
 
 	// Test Wave
 	/*
